8e8f2e70a75f8a11f07e062de1fdb41d9d0f5a74
nikitosabrikos <nikitosabrikos985@gmail.com>
2025-12-22 17:25:58 +0300

    sth

--- config/config.py
+++ config/config.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
-from environs import Env
 import os
+
+LOG_LEVEL = "INFO"
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 
 @dataclass
@@ -20,15 +22,15 @@ class Config:
     logger: Logger
 
 
-def load_env(path: str | None = None):
-    env = Env()
-    env.read_env(path or ".env")
-    return env
+# def load_env(path: str | None = None):
+#  env = Env()
+#  env.read_env(path or ".env")
+#   return env
 
 
 def load_config(path: str | None = None) -> Config:
-    env = load_env()
+    # env = load_env()
     return Config(
         bot=Bot(token=os.getenv("BOT_TOKEN")),
-        logger=Logger(level=env("LOG_LEVEL"), format=env("LOG_FORMAT")),
+        logger=Logger(level=LOG_LEVEL, format=LOG_FORMAT),
     )
--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from contextlib import asynccontextmanager
 from webhook.webhook import router as webhook_router
 from bot.bot import bot
 from config.url import WEBHOOK_URL
+from logger.logger import logger
+
+logger.info("starting app")
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await bot.set_webhook(WEBHOOK_URL)
     yield
     await bot.delete_webhook()
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(webhook_router)
